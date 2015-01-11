50898290a4de7473ff0b7311898c8e2c1fb941eb
aenima46n2 <aenima46n2@192.168.1.76>
2015-01-11 17:08:53 -0500

    Updates

--- src/main/java/com/spring/restfulsecurity/RestfulAuthenticationFilter2.java
+++ src/main/java/com/spring/restfulsecurity/RestfulAuthenticationFilter2.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -24,46 +25,71 @@ public class RestfulAuthenticationFilter2 extends GenericFilterBean {
 
 	private AuthenticationManager authenticationManager;
 	
+	private final static String AUTH_USERNAME_HEADER = "X-AUTH-USERNAME";
+	private final static String AUTH_PASSWORD_HEADER = "X-AUTH-PASSWORD";
+	private final static String AUTH_TOKEN_HEADER = "X-AUTH-TOKEN";
+	
 	private final static List<GrantedAuthority> ROLES_12 = AuthorityUtils.createAuthorityList("ROLE_ONE", "ROLE_TWO");
 	
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
 		
 		System.out.println("***ENTERED RESTFUL AUTHENTICATION FILTER 2***");
 		HttpServletRequest httpRequest = this.getAsHttpRequest(req);
+		HttpServletResponse httpResponse = this.getAsHttpResponse(resp);
+		
+		//1. First we should check if a username & password exists, if so, validate.
+		//   if the username and password are valid we can return a new auth token.
+		boolean authenticated = checkUsernamePassword(httpRequest, httpResponse);
+		
+		//2. If username & password does not exist or is not valid then check the auth token.
+		if(!authenticated) {
+			authenticated = checkToken(httpRequest, httpResponse);
+		}
+		
+		//3. Always continue down the chain. The checkUsernamePassword and checkToken
+		//   methods will handle the setting of the user in the SecurityContext.  If both
+		//   failed authentication there will be no user in the SecurityContext and if the backend
+		//   process trying to be reached does not allow anonymous, then user will be given an error code
+		//   of unauthorized.
+		chain.doFilter(req, resp);
+	}
+	
+	private boolean checkUsernamePassword(HttpServletRequest httpRequest, HttpServletResponse response) {
+		
+		//Retrieve the credentials from the request header.
+		String username = httpRequest.getHeader(AUTH_USERNAME_HEADER);
+		String password = httpRequest.getHeader(AUTH_PASSWORD_HEADER);
+		
+		//If either do not exist, then cannot authenticate via username & password
+		if(username == null || password == null)
+			return false;
+		
+		UsernamePasswordAuthenticationToken authentication =
+				new UsernamePasswordAuthenticationToken(username, password);
+		Authentication auth = null;
+		try {
+			auth = this.getAuthenticationManager().authenticate(authentication);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		//authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
+		
+		if(auth != null && auth.isAuthenticated()) {
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private boolean checkToken(HttpServletRequest httpRequest, HttpServletResponse response) {
 		
 		//Retrieve the Auth Token from the request header.
 		String authToken = httpRequest.getHeader("X-Auth-Token");
 		System.out.println("***ENTERED RESTFUL AUTHENTICATION FILTER 2 -- AuthToken: "+authToken+"***");
-
-		//If AuthToken does not exist, continue on down the chain
-		//Methods that are not secured will be allowed, otherwise the custom
-		//Entry point will be executed and return an authorization error.
-		if (authToken != null) {
-
-			//TODO: If authToken exists, validate.
-			//IF valid authToken, create the "authentication" (i.e. Spring's UsernamePasswordAuthenticationToken)
-			//and set it in the Security context.
-			//ELSE return authorization error.
-
-			UsernamePasswordAuthenticationToken authentication =
-					new UsernamePasswordAuthenticationToken("mkyong", "123456");
-			Authentication auth = null;
-			try {
-				auth = this.getAuthenticationManager().authenticate(authentication);
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-			//authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
-			
-			if(auth != null && auth.isAuthenticated())
-				SecurityContextHolder.getContext().setAuthentication(authentication);
-		}	
-
-		chain.doFilter(req, resp);
+		
+		return false;
 	}
-	
-	//TODO: Validate an Authentication Token
-	//1. 
 	
 	private HttpServletRequest getAsHttpRequest(ServletRequest request)
 	{
@@ -72,6 +98,15 @@ public class RestfulAuthenticationFilter2 extends GenericFilterBean {
 		}
 
 		return (HttpServletRequest) request;
+	}
+	
+	private HttpServletResponse getAsHttpResponse(ServletResponse response)
+	{
+		if (!(response instanceof HttpServletResponse)) {
+			throw new RuntimeException("Expecting an HTTP response");
+		}
+
+		return (HttpServletResponse) response;
 	}
 
 	public AuthenticationManager getAuthenticationManager() {
